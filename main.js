f81d193daf1182c658499bd11957dd3ad955581c
bqeesa <ajanibaliqeesbusiness@gmail.com>
2026-09-11 15:25:30 +0100

    changed javascript

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ console.log(name)
 function add(){
 
     console.log("Welcome to my site")
-    
+
+}
+
+let age = 20
+if (age>=18){
+    console.log("You are allowed access")
+}else{
+    console.log("  Wait till age 18")
 }
